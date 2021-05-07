e9eff412447f4d5bc322b9169bf340e974f4dff4
chenzh-pc <czh533143@163.com>
2021-05-07 21:09:44 +0800

    feat: jsdoc初始化测试

---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: 123
+ */
+
+
+/**
+ * @description: 
+ * @param {*} name
+ * @param {*} age
+ * @return {*}
+ */
+ function test({name,age}){
+    return name+age
+}
---
+++ src/view/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-07 20:29:09
+ * @LastEditTime: 2021-05-07 21:05:49
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \jsdoc-demo\test.js
+ */
+
+
+
+ function test({name,age}){
+    return name+age
+}
